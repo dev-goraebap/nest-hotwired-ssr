c99abcd9576cef22a3f0ec7e4036a6bcc710aed8
dev.goraebap <dev.goraebap@gmail.com>
2025-04-18 19:39:19 +0900

    feat: Add font management with OpenSans and GothicA1, and update static asset handling

--- src/main.ts
+++ src/main.ts
@@ -20,6 +20,9 @@ async function bootstrap() {
   app.useStaticAssets(join(process.cwd(), 'resources', 'imgs'), {
     prefix: '/imgs',
   });
+  app.useStaticAssets(join(process.cwd(), 'resources', 'fonts'), {
+    prefix: '/fonts',
+  });
 
   await app.listen(process.env.PORT ?? 3000);
 }
