cb1ce012263d9ea015ab90b06d96ec25b01ff64a
dev-goraebap <dev.goraebap@gmail.com>
2025-06-23 17:23:36 +0900

    feat:  html 파일 변경시 바로 적용되지 않음 -> nunjucks 템플릿엔진은 별도로 watch 옵션을 사용하여 파일 변경 감지 활성화 필요

--- src/main.ts
+++ src/main.ts
@@ -12,7 +12,10 @@ async function bootstrap() {
    * 템플릿 엔진 설정(모질라제단에서 만든 nunjucks 사용)
    *-------------------------------------------------------------------------*/
   const views = join(process.cwd(), 'resources', 'views');
-  nunjucks.configure(views, { express: app.getHttpAdapter().getInstance() });
+  nunjucks.configure(views, { 
+    express: app.getHttpAdapter().getInstance(),
+    watch: true, // 템플릿 파일 변경 감지 활성화
+  });
   app.setBaseViewsDir(views);
   app.setViewEngine('html');
 
