07ddf3cc70aa6fe11c3a1ee793686eff250987a4
dev-goraebap <dev.goraebap@gmail.com>
2025-06-23 17:15:54 +0900

    feat: nunjucks 템플릿 엔진 설정 추가

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,21 @@
 import { NestFactory } from '@nestjs/core';
+import { NestExpressApplication } from '@nestjs/platform-express';
+import * as nunjucks from 'nunjucks';
+import { join } from 'path';
+
 import { AppModule } from './app.module';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create<NestExpressApplication>(AppModule);
+
+  /**-------------------------------------------------------------------------
+   * 템플릿 엔진 설정(모질라제단에서 만든 nunjucks 사용)
+   *-------------------------------------------------------------------------*/
+  const views = join(process.cwd(), 'resources', 'views');
+  nunjucks.configure(views, { express: app.getHttpAdapter().getInstance() });
+  app.setBaseViewsDir(views);
+  app.setViewEngine('html');
+
   await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();
