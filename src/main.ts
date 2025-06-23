8b48e3bfcdeb0d701b7e79a04784d50dd59b374b
dev-goraebap <dev.goraebap@gmail.com>
2025-06-23 17:35:04 +0900

    feat: nunjucks 템플릿 엔진 설정 업데이트 - autoescape 및 noCache 옵션 추가

--- src/main.ts
+++ src/main.ts
@@ -14,7 +14,12 @@ async function bootstrap() {
   const views = join(process.cwd(), 'resources', 'views');
   nunjucks.configure(views, { 
     express: app.getHttpAdapter().getInstance(),
+    // 명시 안해도 기본값은 true
+    // 사용자 입력이나 데이터베이스에서 가져온 값이 템플릿에 삽입될 때 HTML 특수 문자(<, >, &, ", ' 등)를 자동으로 이스케이프 처리
+    // -> XSS 공격 방지
+    autoescape: true,
     watch: true, // 템플릿 파일 변경 감지 활성화
+    noCache: process.env.NODE_ENV !== 'production' // 개발 환경에서는 캐시 비활성화
   });
   app.setBaseViewsDir(views);
   app.setViewEngine('html');
