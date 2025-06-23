701b259abb1fd4209d2ac089f4e2cac2067b9064
dev-goraebap <dev.goraebap@gmail.com>
2025-06-24 02:45:02 +0900

    feat: 실험실 페이지를 위한 LabController 및 index.html 추가

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,12 @@
 import { Module } from '@nestjs/common';
 import { HomeController } from './controllers/home.controller';
+import { LabController } from './controllers/lab.controller';
 
 @Module({
   imports: [],
   controllers: [
-    HomeController
+    HomeController,
+    LabController
   ]
 })
 export class AppModule {}
