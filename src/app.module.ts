efed63c2ed52857f9cf6b36b4ea4c9e258e7ead1
dev-goraebap <dev.goraebap@gmail.com>
2025-06-25 01:51:49 +0900

    feat: 홈 및 실험실 페이지 내용 추가, 모달 예제 파일 생성 및 불필요한 LabController 삭제

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,13 +1,11 @@
 import { Module } from '@nestjs/common';
 import { HomeController } from './controllers/home.controller';
-import { LabController } from './controllers/lab.controller';
 import { ModalExampleController } from './controllers/lab/modal-example.controller';
 
 @Module({
   imports: [],
   controllers: [
     HomeController,
-    LabController,
     ModalExampleController
   ]
 })
