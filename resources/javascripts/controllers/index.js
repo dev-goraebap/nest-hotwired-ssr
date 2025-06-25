b1ec383bd473423b1e54a14b4ffee29e679707e8
dev-goraebap <dev.goraebap@gmail.com>
2025-06-25 14:54:40 +0900

    feat: 예제용 모달01 컨트롤러 추가 및 인덱스에 등록

--- resources/javascripts/controllers/index.js
+++ resources/javascripts/controllers/index.js
@@ -1,8 +1,10 @@
 import { application } from "./application";
 import { HelloController } from "./hello.controller";
+import { ModalController as Modal01Controller } from "./lab/modal-example-01/modal.controller";
 import { CsrModalController } from "./modal/csr-modal.controller";
 import { SsrModalController } from "./modal/ssr-modal.controller";
 
 application.register("hello", HelloController);
+application.register("modal-01", Modal01Controller);
 application.register("csr-modal", CsrModalController);
 application.register("ssr-modal", SsrModalController);
