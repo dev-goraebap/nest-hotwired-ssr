2245f3fea59bdf6f20927cda41f42458e3c6bd7d
dev-goraebap <dev.goraebap@gmail.com>
2025-06-25 19:10:51 +0900

    feat: 모달 예제 3 추가 및 기존 모달 관련 코드 정리

--- resources/javascripts/controllers/index.js
+++ resources/javascripts/controllers/index.js
@@ -1,10 +1,6 @@
 import { application } from "./application";
-import { HelloController } from "./hello.controller";
 import { ModalController as Modal01Controller } from "./lab/modal-example-01/modal.controller";
-import { CsrModalController } from "./modal/csr-modal.controller";
-import { SsrModalController } from "./modal/ssr-modal.controller";
+import { ModalController as Modal03Controller } from "./lab/modal-example-03/modal.controller";
 
-application.register("hello", HelloController);
 application.register("modal-01", Modal01Controller);
-application.register("csr-modal", CsrModalController);
-application.register("ssr-modal", SsrModalController);
+application.register("modal-03", Modal03Controller);
---
+++ src/controllers/lab/modal-example-03.controller.ts
@@ -0,0 +1,36 @@
+import { Controller, Get, Req, Res } from '@nestjs/common';
+import { Request, Response } from 'express';
+import { EdgeJsAdapter } from 'src/shared/edge-js';
+
+@Controller({ path: 'lab/modal-example-03' })
+export class ModalExample03Controller {
+  @Get()
+  index(@Res() res: Response) {
+    return EdgeJsAdapter.render(res, 'page::lab/modal-example-03/index');
+  }
+
+  @Get('content')
+  content(@Req() req: Request, @Res() res: Response) {
+    const data = {
+      title: 'hello world',
+      content:
+        'Ea mollit consectetur qui duis laborum irure. Sunt elit ex irure duis non irure irure aute aliquip consectetur elit commodo tempor. Duis eiusmod est nulla incididunt in excepteur ad eu ea exercitation.',
+    };
+    return EdgeJsAdapter.renderOnlyTurboRequest(
+      req,
+      res,
+      'page::lab/modal-example-03/_content',
+      data,
+    );
+  }
+
+  @Get('lazy-content')
+  async lazyContent(@Req() req: Request, @Res() res: Response) {
+    await new Promise((resolve) => {
+      setTimeout(() => {
+        resolve(null);
+      }, 1000);
+    });
+    return this.content(req, res);
+  }
+}
