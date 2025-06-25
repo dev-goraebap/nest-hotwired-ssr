96f8d9850d952986f4a7052728de8c026f147d62
dev-goraebap <dev.goraebap@gmail.com>
2025-06-26 03:12:39 +0900

    feat: EdgeJsModule 을 메인 모듈에서 참조하는 방식으로 구성

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,9 +4,12 @@ import { FlashExample01Controller } from './controllers/lab/flash-example-01.con
 import { ModalExample01Controller } from './controllers/lab/modal-example-01.controller';
 import { ModalExample02Controller } from './controllers/lab/modal-example-02.controller';
 import { ModalExample03Controller } from './controllers/lab/modal-example-03.controller';
+import { EdgeJsModule } from './shared/edge-js';
 
 @Module({
-  imports: [],
+  imports: [
+    EdgeJsModule.forRootAsync()
+  ],
   controllers: [
     HomeController,
     ModalExample01Controller,
