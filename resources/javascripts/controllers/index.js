82e02378fbe62bc65f5451514d8e7e686b5468e9
dev-goraebap <dev.goraebap@gmail.com>
2025-06-27 08:39:35 +0900

    feat: 테마 스위처 컨트롤러 추가

--- resources/javascripts/controllers/index.js
+++ resources/javascripts/controllers/index.js
@@ -2,7 +2,9 @@ import { application } from "./application";
 import { FlashController as Flash02Controller } from "./lab/flash-example-02/flash.controller";
 import { ModalController as Modal01Controller } from "./lab/modal-example-01/modal.controller";
 import { ModalController as Modal03Controller } from "./lab/modal-example-03/modal.controller";
+import { ThemeSwitcherController } from "./lab/theme-switcher-example/theme-switcher.controller";
 
 application.register("modal-01", Modal01Controller);
 application.register("modal-03", Modal03Controller);
 application.register("flash-02", Flash02Controller);
+application.register("theme-switcher", ThemeSwitcherController);
