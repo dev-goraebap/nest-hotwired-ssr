d244b92a35cae952c3db69cf89157ab6c2c2d58c
dev-goraebap <dev.goraebap@gmail.com>
2025-06-29 22:29:34 +0900

    feat: typeorm-active-storage 모듈의 엔트리 포인트 추가

---
+++ src/shared/typeorm-active-storage/index.ts
@@ -0,0 +1,4 @@
+export * from './services/active-storage.service';
+export * from './typeorm-active-storage.module';
+export * from './entities/attachment.entity';
+export * from './entities/blob.entity';
