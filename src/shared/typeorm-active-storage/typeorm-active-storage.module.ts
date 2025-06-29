c8efe41976e96c6a7fb9f662b3215521532a061d
dev-goraebap <dev.goraebap@gmail.com>
2025-06-29 22:47:24 +0900

    style: TypeORM 모듈 및 엔티티 임포트 순서 정리

--- src/shared/typeorm-active-storage/typeorm-active-storage.module.ts
+++ src/shared/typeorm-active-storage/typeorm-active-storage.module.ts
@@ -1,11 +1,11 @@
 import { DynamicModule, Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
+import { AttachmentEntity } from './entities/attachment.entity';
+import { BlobEntity } from './entities/blob.entity';
 import { LocalStorageAdapter } from './infra/adapters/local-storage.adapter';
 import { STORAGE_PORT } from './infra/ports/storage.port';
 import { ActiveStorageService } from './services/active-storage.service';
-import { TypeOrmModule } from '@nestjs/typeorm';
-import { BlobEntity } from './entities/blob.entity';
-import { AttachmentEntity } from './entities/attachment.entity';
 
 @Module({})
 export class TypeormActiveStorageModule {
