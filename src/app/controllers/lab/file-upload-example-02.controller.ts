0ddbe6eb508426cf984f18731e4d2d0bfde556b2
dev-goraebap <dev.goraebap@gmail.com>
2025-07-01 14:49:48 +0900

    feat: 파일 업로드 기능 추가 및 이미지 메타데이터 처리

--- src/app/controllers/lab/file-upload-example-02.controller.ts
+++ src/app/controllers/lab/file-upload-example-02.controller.ts
@@ -1,13 +1,63 @@
-import { Controller, Get, Res } from '@nestjs/common';
+import {
+  Controller,
+  Get,
+  Post,
+  Res,
+  UploadedFile,
+  UseInterceptors,
+} from '@nestjs/common';
+import { FileInterceptor } from '@nestjs/platform-express';
 import { Response } from 'express';
 
 import { EdgeView, View } from 'src/shared/edge-in-nest';
+import { GoogleVisionService } from 'src/shared/google-vision';
+import { ActiveStorageService } from 'src/shared/typeorm-active-storage';
 
 @Controller({ path: 'lab/file-upload-example-02' })
 export class FileUploadExample02Controller {
+  constructor(
+    private readonly googleVision: GoogleVisionService,
+    private readonly activeStorage: ActiveStorageService,
+  ) {}
+
   @Get()
   async index(@View() view: EdgeView, @Res() res: Response) {
-    const template = await view.render('pages::lab/file-upload-example-02/index');
+    const attachments = await this.activeStorage.findAttachmentsByRecord(
+      'file-upload-example-02',
+      '0000',
+      'image',
+    );
+
+    const template = await view.render(
+      'pages::lab/file-upload-example-02/index',
+      {
+        attachments,
+      },
+    );
     return res.send(template);
+  }
+
+  @Post()
+  @UseInterceptors(FileInterceptor('file'))
+  async create(
+    @UploadedFile() file: Express.Multer.File,
+    @View() view: EdgeView,
+    @Res() res: Response,
+  ) {
+    const result = await this.googleVision.extractColors(file.buffer);
+    const dominantColor = result[0]?.hex;
+    console.log(dominantColor);
+    const attachment = await this.activeStorage.attach(
+      file,
+      'file-upload-example-02',
+      '0000',
+      'image',
+      'append'
+    );
+    await this.activeStorage.updateBlobMetadata(attachment.blob, {
+      dominantColor,
+    });
+    view.setFlash('notice', '이미지 업로드 성공');
+    return res.redirect('/lab/file-upload-example-02');
   }
 }
