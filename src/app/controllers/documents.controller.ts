99c5d7292ca763bb28a08d093557d2736455e1cb
dev-goraebap <dev.goraebap@gmail.com>
2025-07-04 00:17:17 +0900

    feat: 문서 생성, 수정, 삭제 기능 개선 및 예외 처리 추가

--- src/app/controllers/documents.controller.ts
+++ src/app/controllers/documents.controller.ts
@@ -1,10 +1,15 @@
 import {
   Body,
   Controller,
+  Delete,
   Get,
+  NotFoundException,
+  Param,
+  ParseIntPipe,
   Post,
+  Put,
   Res,
-  UseInterceptors
+  UseInterceptors,
 } from '@nestjs/common';
 import { Response } from 'express';
 
@@ -14,6 +19,7 @@ import {
   View,
 } from 'src/shared/edge-in-nest';
 
+import { DocumentEntity } from '../entities/document.entity';
 import { DocumentsService } from '../services/documents.service';
 
 @Controller({ path: 'documents' })
@@ -36,13 +42,49 @@ export class DocumentsController {
   @Post()
   @UseInterceptors(CrsfProtectedInterceptor)
   async create(@Body() dto: any, @View() view: EdgeView, @Res() res: Response) {
-    await this.documentsService.create(dto);
+    await this.documentsService.create(dto?.document);
     view.setFlash('notice', '게시물이 작성되었습니다.');
     return res.redirect('/documents');
   }
 
   @Get(':id/edit')
-  async edit(@View() view: EdgeView) {
-    return await view.render('pages/documents/edit');
+  async edit(@Param('id', ParseIntPipe) id: number, @View() view: EdgeView) {
+    const document = await DocumentEntity.findOne({
+      where: { id },
+      relations: {
+        category: true,
+      },
+    });
+    if (!document) {
+      throw new NotFoundException('문서를 찾을 수 없습니다.');
+    }
+    return await view.render('pages/documents/edit', { document });
+  }
+
+  @Put(':id')
+  @UseInterceptors(CrsfProtectedInterceptor)
+  async update(
+    @Param('id', ParseIntPipe) id: number,
+    @Body() dto: any,
+    @View() view: EdgeView,
+    @Res() res: Response,
+  ) {
+    console.log('요청옴!!');
+    console.log(dto);
+    console.log(id);
+    await this.documentsService.update(id, dto?.document);
+    view.setFlash('notice', '게시물이 업데이트되었습니다.');
+    return res.redirect(303, '/documents');
+  }
+
+  @Delete(':id')
+  async destroy(
+    @Param('id', ParseIntPipe) id: number,
+    @View() view: EdgeView,
+    @Res() res: Response,
+  ) {
+    await this.documentsService.destroy(id);
+    view.setFlash('notice', '게시물이 삭제되었습니다.');
+    return res.redirect(303, '/documents');
   }
 }
--- src/app/services/documents.service.ts
+++ src/app/services/documents.service.ts
@@ -6,11 +6,20 @@ import { DocumentEntity } from '../entities/document.entity';
 @Injectable()
 export class DocumentsService {
   index() {
-    return DocumentEntity.find();
+    return DocumentEntity.find({
+      relations: {
+        category: true,
+      },
+    });
   }
 
   async create(dto: any) {
-    const { title, content, slug, categoryId } = dto;
+    const { title, content, slug, category: ctg } = dto;
+    const categoryId = ctg?.id;
+    if (!title) throw new BadRequestException('제목을 입력해 주세요.');
+    if (!content) throw new BadRequestException('내용을 입력해 주세요.');
+    if (!slug) throw new BadRequestException('슬러그를 만들어 주세요.');
+    if (!categoryId) throw new BadRequestException('카테고리를 선택해 주세요.');
 
     const category = await CategoryEntity.findOne({
       where: { id: categoryId },
@@ -36,5 +45,57 @@ export class DocumentsService {
       category,
     });
     await document.save();
+  }
+
+  async update(id: number, dto: any) {
+    const { title, content, slug, category: ctg } = dto;
+    const categoryId = ctg?.id;
+    if (!title) throw new BadRequestException('제목을 입력해 주세요.');
+    if (!content) throw new BadRequestException('내용을 입력해 주세요.');
+    if (!slug) throw new BadRequestException('슬러그를 만들어 주세요.');
+    if (!categoryId) throw new BadRequestException('카테고리를 선택해 주세요.');
+
+    const category = await CategoryEntity.findOne({
+      where: { id: categoryId },
+    });
+    if (!category) {
+      throw new BadRequestException('카테고리를 찾을 수 없습니다.');
+    }
+
+    let document = await DocumentEntity.findOne({ where: { id } });
+    if (!document) {
+      throw new BadRequestException('문서를 찾을 수 없습니다.');
+    }
+
+    if (title !== document.title) {
+      const hasTitle = await DocumentEntity.exists({ where: { title } });
+      if (hasTitle) throw new BadRequestException('중복되는 제목 입니다');
+    }
+
+    if (slug !== document.slug) {
+      const hasSlug = await DocumentEntity.exists({ where: { slug } });
+      if (hasSlug) throw new BadRequestException('사용중인 슬러그 입니다');
+    }
+    
+    document = DocumentEntity.create({
+      ...document,
+      title,
+      content,
+      slug,
+      category: {
+        id: categoryId,
+      },
+    });
+    await document.save();
+  }
+
+  async destroy(id: number) {
+    const document = await DocumentEntity.findOne({
+      where: { id },
+    });
+    if (!document) {
+      throw new BadRequestException('문서를 찾을 수 없습니다.');
+    }
+    await document.remove();
   }
 }
