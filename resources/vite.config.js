b9c5ea85e5621b86ef6506bd75b7b89b01e1bc3e
dev-goraebap <dev.goraebap@gmail.com>
2025-07-04 16:45:24 +0900

    fix: Vite 설정에서 파일명 해시 추가 및 manifest.json 생성 설정

--- resources/vite.config.js
+++ resources/vite.config.js
@@ -12,6 +12,8 @@ export default defineConfig({
     }),
   ],
   build: {
+    // manifest.json 파일 생성
+    manifest: true,
     outDir: 'assets/builds',
     rollupOptions: {
       input: {
@@ -19,9 +21,10 @@ export default defineConfig({
         tailwind: resolve(__dirname, 'stylesheets/tailwind.css')
       },
       output: {
-        entryFileNames: '[name].js',
-        chunkFileNames: '[name].js',
-        assetFileNames: '[name].[ext]',
+        // 해시가 포함된 파일명으로 변경
+        entryFileNames: '[name]-[hash].js',
+        chunkFileNames: '[name]-[hash].js',
+        assetFileNames: '[name]-[hash].[ext]',
       },
     },
   },
