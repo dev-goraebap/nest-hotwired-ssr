350602274974f624bf1830c8f19e6e7bf0e59066
dev-goraebap <dev.goraebap@gmail.com>
2025-07-05 00:00:28 +0900

    refactor: Vite 설정 파일에서 경로 및 내용 수정

--- resources/vite.config.js
+++ resources/vite.config.js
@@ -1,24 +1,20 @@
-import { defineConfig } from 'vite';
-import tailwindcss from '@tailwindcss/vite'
+import tailwindcss from '@tailwindcss/vite';
 import { resolve } from 'path';
+import { defineConfig } from 'vite';
 
 export default defineConfig({
   plugins: [
     tailwindcss({
-      content: [
-        './views/**/*.edge',
-        './javascripts/**/*.js',
-      ],
+      content: ['./views/**/*.edge', './src/**/*.js'],
     }),
   ],
   build: {
-    // manifest.json 파일 생성
     manifest: true,
-    outDir: 'assets/builds',
+    outDir: 'public/builds',
     rollupOptions: {
       input: {
-        app: resolve(__dirname, 'javascripts/app.js'),
-        tailwind: resolve(__dirname, 'stylesheets/tailwind.css')
+        app: resolve(__dirname, 'src/app.js'),
+        tailwind: resolve(__dirname, 'src/tailwind.css'),
       },
       output: {
         // 해시가 포함된 파일명으로 변경
@@ -33,4 +29,5 @@ export default defineConfig({
       usePolling: true,
     },
   },
+  publicDir: false,
 });
