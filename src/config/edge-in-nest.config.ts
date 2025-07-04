a4a9eaf6ba4b2d102190e024a231471e8ef829a6
dev-goraebap <dev.goraebap@gmail.com>
2025-07-04 16:45:20 +0900

    feat: EdgeInNestOptions에 assets 속성 추가

--- src/config/edge-in-nest.config.ts
+++ src/config/edge-in-nest.config.ts
@@ -19,6 +19,17 @@ export class EdgeInNestConfig implements EdgeInNestOptionsFactory {
       disks: [],
       // cache: process.env.NODE_ENV === 'production',
       cache: false,
+      assets: {
+        manifest: join(
+          process.cwd(),
+          'resources',
+          'assets',
+          'builds',
+          '.vite',
+          'manifest.json',
+        ),
+        assetsBaseUrl: '/public/builds',
+      },
     };
   }
 }
