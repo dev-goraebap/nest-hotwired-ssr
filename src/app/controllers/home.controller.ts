62af886d9bb0b404cdbc2512a186250d29e0ef93
dev-goraebap <dev.goraebap@gmail.com>
2025-07-04 15:25:24 +0900

    feat: README 파일을 사용하여 마크다운 HTML 렌더링 개선

--- src/app/controllers/home.controller.ts
+++ src/app/controllers/home.controller.ts
@@ -1,19 +1,14 @@
 import { Controller, Get, Query } from '@nestjs/common';
 
 import { EdgeView, View } from 'src/shared/edge-in-nest';
-
 import { getMarkdownHtml } from '../helpers/markdown';
 
 @Controller({ path: '' })
 export class HomeController {
   @Get()
-  async index(
-    @View() view: EdgeView,
-    @Query('message') message?: string,
-  ) {
-    const markdownHtml = getMarkdownHtml('home');
+  async index(@View() view: EdgeView, @Query('message') message?: string) {
+    const markdownHtml = getMarkdownHtml('README');
     return await view.render('pages/home/index', {
-      message: message || 'hello world',
       markdownHtml,
     });
   }
--- src/app/helpers/markdown.ts
+++ src/app/helpers/markdown.ts
@@ -4,7 +4,7 @@ import { join } from 'path';
 
 export function getMarkdownHtml(filename: string) {
   const markdown = readFileSync(
-    join(process.cwd(), 'resources', 'docs', filename + '.md'),
+    join(process.cwd(), filename + '.md'),
     'utf-8',
   );
   return marked.parse(markdown);
