04899b1ea71d65ce8e63f85ff6e3ca3f5b9c89f5
dev-goraebap <dev.goraebap@gmail.com>
2025-07-04 17:30:46 +0900

    refactor: 테마 스위처 기능 개선을 위한 폼 제출 방식 변경 및 불필요한 코드 제거

--- resources/javascripts/controllers/lab/theme-switcher-example/theme-switcher.controller.js
+++ resources/javascripts/controllers/lab/theme-switcher-example/theme-switcher.controller.js
@@ -1,24 +1,14 @@
 import { Controller } from '@hotwired/stimulus';
 
 export class ThemeSwitcherController extends Controller {
-  static targets = ['button'];
-
-  onChangeTheme(e) {
-    const target = e.currentTarget;
-    const theme = target.dataset.theme;
+  beforeSubmit(event) {
+    console.log(
+      'Form is about to be submitted for theme:',
+      event.target.querySelector('input[name="theme"]').value,
+    );
 
     // 전체 html 테마 변경
+    const theme = event.target.querySelector('input[name="theme"]').value;
     document.documentElement.setAttribute('data-theme', theme);
-
-    // 쿠키에 테마 저장 (1달 유효)
-    const oneMonthInSeconds = 30 * 24 * 60 * 60; // 30일을 초로 환산
-    document.cookie = `theme=${theme}; path=/; max-age=${oneMonthInSeconds}`;
-
-    // 모든 버튼들 강조효과 제거
-    this.buttonTargets.forEach((x) => {
-      x.classList.remove('border-primary', 'border-2');
-    });
-    // 선택된 버튼에 강조효과 추가
-    target.classList.add('border-primary', 'border-2');
   }
 }
