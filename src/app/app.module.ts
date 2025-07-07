ed651756a67b3b2877540d3cbe2002fcaa1eda4e
dev-goraebap <dev.goraebap@gmail.com>
2025-07-07 21:26:11 +0900

    feat: AppController 및 AppService 제거, AppModule에서 관련 의존성 삭제

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,8 +1,6 @@
 import { Module } from '@nestjs/common';
 import { NestMvcCoreModule } from 'nestjs-mvc-tools';
 
-import { AppController } from '../app.controller';
-import { AppService } from '../app.service';
 import { AdminController } from './controllers/admin.controller';
 import { CategoriesController } from './controllers/admin/categories.controller';
 import { DocumentsController as AdminDocumentsController } from './controllers/admin/documents.controller';
@@ -11,12 +9,11 @@ import { SessionsController } from './controllers/sessions.controller';
 @Module({
   imports: [NestMvcCoreModule.forRoot({ debug: true })],
   controllers: [
-    AppController,
     AdminController,
     AdminDocumentsController,
     CategoriesController,
     SessionsController
   ],
-  providers: [AppService],
+  providers: [],
 })
 export class AppModule {}
