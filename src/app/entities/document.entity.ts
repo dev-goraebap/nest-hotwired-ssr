a1e3e3a6538f3a5a569ae0e110c829c3bb877692
dev-goraebap <dev.goraebap@gmail.com>
2025-07-09 00:02:37 +0900

    feat: 카테고리 및 문서 엔티티 추가

---
+++ src/app/entities/document.entity.ts
@@ -0,0 +1,48 @@
+import {
+  BaseEntity,
+  Column,
+  CreateDateColumn,
+  Entity,
+  JoinColumn,
+  ManyToOne,
+  PrimaryGeneratedColumn,
+  UpdateDateColumn,
+} from 'typeorm';
+import { CategoryEntity } from './category.entity';
+
+@Entity({ name: 'documents' })
+export class DocumentEntity extends BaseEntity {
+  @PrimaryGeneratedColumn()
+  readonly id: number;
+
+  @Column({ unique: true })
+  readonly slug: string;
+
+  @Column({ unique: true })
+  readonly title: string;
+
+  @Column({ type: 'text' })
+  readonly content: string;
+
+  @CreateDateColumn()
+  readonly createdAt: Date;
+
+  @UpdateDateColumn()
+  readonly updatedAt: Date;
+
+  @ManyToOne(() => CategoryEntity, (e) => e.documents, {
+    nullable: true,
+    onDelete: 'SET NULL',
+  })
+  @JoinColumn({ name: 'category_id' })
+  readonly category: CategoryEntity;
+
+  getUrl() {
+    console.log(this.slug);
+    if (this.slug.includes('/')) {
+      return `/${this.slug}`;
+    } else {
+      return `/documents/${this.slug}`;
+    }
+  }
+}
