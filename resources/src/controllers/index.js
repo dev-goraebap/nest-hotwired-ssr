3ba0eab090e7bd9b64ec069692124e6dd881ee38
dev-goraebap <dev.goraebap@gmail.com>
2025-07-08 22:54:17 +0900

    feat: import 문 정리 및 DocumentFormController 등록

--- resources/src/controllers/index.js
+++ resources/src/controllers/index.js
@@ -1,4 +1,11 @@
-import { application } from "./application";
-import { HelloWorldController } from "./hello_world_controller";
+import { application } from './application';
 
-application.register("hello", HelloWorldController);
+import { DocumentFormController } from './admin/documents/form.controller';
+import { HelloWorldController } from './hello_world_controller';
+import { ModalController } from './modal.controller';
+import { TinyMceController } from './tiny-mce.controller';
+
+application.register('hello', HelloWorldController);
+application.register('tiny-mce', TinyMceController);
+application.register('modal', ModalController);
+application.register('document-form', DocumentFormController);
