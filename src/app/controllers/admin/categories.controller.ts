d44dadc7cef730af9fb4ffa9ef407f604c2f5702
dev-goraebap <dev.goraebap@gmail.com>
2025-07-09 01:27:10 +0900

    feat: 카테고리 컨트롤러에서 create 및 destroy 메서드 수정 및 리팩토링

--- src/app/controllers/admin/categories.controller.ts
+++ src/app/controllers/admin/categories.controller.ts
@@ -6,16 +6,27 @@ import {
   ParseIntPipe,
   Post,
   Put,
+  Req,
   Res,
 } from '@nestjs/common';
 import { Response } from 'express';
-import { Flash, NestMvcFlash, NestMvcView, View } from 'nestjs-mvc-tools';
+import {
+  Flash,
+  NestMvcFlash,
+  NestMvcReq,
+  NestMvcView,
+  View,
+} from 'nestjs-mvc-tools';
+import { CategoriesService } from 'src/app/services/categories.service';
 
 @Controller({ path: 'admin/categories' })
 export class CategoriesController {
+  constructor(private readonly categoriesService: CategoriesService) {}
+
   @Get()
   async index(@View() view: NestMvcView) {
-    return view.render('pages/admin/categories/index', {});
+    const categories = await this.categoriesService.index();
+    return view.render('pages/admin/categories/index', { categories });
   }
 
   @Get('new')
@@ -24,9 +35,12 @@ export class CategoriesController {
   }
 
   @Post()
-  async create(@Flash() flash: NestMvcFlash, @Res() res: Response) {
-    flash.success('작업 성공');
-    return res.redirect('/admin/categories');
+  async create(@Req() req: NestMvcReq) {
+    console.log(req.body?.category);
+    await this.categoriesService.create(req.body?.category);
+
+    req.flash.success('작업 성공');
+    return req.view.render('pages/admin/categories/_success');
   }
 
   @Get(':id/edit')
@@ -50,7 +64,8 @@ export class CategoriesController {
     @Flash() flash: NestMvcFlash,
     @Res() res: Response,
   ) {
+    await this.categoriesService.destroy(id);
     flash.success('삭제 성공');
-    return res.redirect('/admin/categories');
+    return res.redirect(303, '/admin/categories');
   }
 }
