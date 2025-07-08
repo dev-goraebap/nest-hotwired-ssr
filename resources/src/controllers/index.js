904f5549a0be0d1d1d045ccd6624ec8534318344
dev-goraebap <dev.goraebap@gmail.com>
2025-07-09 01:27:18 +0900

    feat: FlashController 추가 및 등록

--- resources/src/controllers/index.js
+++ resources/src/controllers/index.js
@@ -1,6 +1,7 @@
 import { application } from './application';
 
 import { DocumentFormController } from './admin/documents/form.controller';
+import { FlashController } from './flash.controller';
 import { HelloWorldController } from './hello_world_controller';
 import { ModalController } from './modal.controller';
 import { TinyMceController } from './tiny-mce.controller';
@@ -9,3 +10,4 @@ application.register('hello', HelloWorldController);
 application.register('tiny-mce', TinyMceController);
 application.register('modal', ModalController);
 application.register('document-form', DocumentFormController);
+application.register('flash', FlashController);
