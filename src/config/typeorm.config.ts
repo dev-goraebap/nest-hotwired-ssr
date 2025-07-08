58bbed9e000476b76a91670b5e0b820d816adb50
dev-goraebap <dev.goraebap@gmail.com>
2025-07-09 00:03:20 +0900

    feat: TypeORM 설정을 위한 TypeOrmConfig 클래스 추가

---
+++ src/config/typeorm.config.ts
@@ -0,0 +1,18 @@
+import { Injectable } from '@nestjs/common';
+import { TypeOrmModuleOptions, TypeOrmOptionsFactory } from '@nestjs/typeorm';
+import { join } from 'path';
+
+@Injectable()
+export class TypeOrmConfig implements TypeOrmOptionsFactory {
+
+  createTypeOrmOptions(): Promise<TypeOrmModuleOptions> | TypeOrmModuleOptions {
+    return {
+      type: 'sqlite',
+      database: join(process.cwd(), 'tmp', 'development.sqlite'),
+      synchronize: true,
+      autoLoadEntities: true,
+      logger: 'debug',
+      logging: true,
+    };
+  }
+}
