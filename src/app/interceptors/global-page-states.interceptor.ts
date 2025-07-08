dd380d85621fed66c6517a698743e6f3ee446dc1
dev-goraebap <dev.goraebap@gmail.com>
2025-07-08 20:56:58 +0900

    feat: GlobalPageStatesInterceptor 추가 및 언어 처리 로직 구현

---
+++ src/app/interceptors/global-page-states.interceptor.ts
@@ -0,0 +1,35 @@
+import {
+  CallHandler,
+  ExecutionContext,
+  Injectable,
+  NestInterceptor,
+} from '@nestjs/common';
+import { Request } from 'express';
+import { I18nContext } from 'nestjs-i18n';
+import { NestMvcView } from 'nestjs-mvc-tools';
+import { Observable } from 'rxjs';
+
+@Injectable()
+export class GlobalPageStatesInterceptor implements NestInterceptor {
+  intercept(
+    context: ExecutionContext,
+    next: CallHandler<any>,
+  ): Observable<any> | Promise<Observable<any>> {
+    // View 인스턴스 가져오기
+    const req: Request = context.switchToHttp().getRequest();
+    const view = req['view'] as NestMvcView;
+
+    if (!view) {
+      throw new Error('Edge View 인스턴스가 초기화되지않음');
+    }
+
+    // 현재 요청의 언어 가져오기
+    const i18nContext = I18nContext.current();
+    const lang = i18nContext?.lang;
+
+    // 전역 상태로 lang 처리
+    view.share({ lang });
+
+    return next.handle();
+  }
+}
