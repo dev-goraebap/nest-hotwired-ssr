f438190e12ea41bb9595367637010743d6d6bc95
dev-goraebap <dev.goraebap@gmail.com>
2025-07-10 16:40:48 +0900

    feat: getSidebarCategories 메서드에 언어 코드 매개변수 추가 및 번역 처리 구현

--- src/app/services/categories.service.ts
+++ src/app/services/categories.service.ts
@@ -1,20 +1,31 @@
 import { CategoryEntity } from '../entities/category.entity';
 
 export class CategoriesService {
-  async getSidebarCategories() {
-    const results = await CategoryEntity.createQueryBuilder('category')
-      .leftJoinAndSelect('category.documents', 'document')
-      .select([
-        'category.id',
-        'category.name',
-        'document.id',
-        'document.title',
-        'document.slug',
-      ])
-      .orderBy('category.order', 'ASC')
-      .addOrderBy('category.createdAt', 'DESC')
-      .addOrderBy('document.createdAt', 'ASC')
-      .getMany();
-    return results;
+  async getSidebarCategories(languageCode: string = 'en') {
+    return await CategoryEntity.find({
+      relations: {
+        translations: true,
+        documents: {
+          translations: true,
+        },
+      },
+      where: {
+        translations: {
+          languageCode,
+        },
+        documents: {
+          translations: {
+            languageCode,
+          },
+        },
+      },
+      order: {
+        order: 'ASC',
+        createdAt: 'DESC',
+        documents: {
+          createdAt: 'ASC',
+        },
+      },
+    });
   }
 }
