da4467e8cf1277bf8663cce2fc5bd884dde7acdb
dev-goraebap <dev.goraebap@gmail.com>
2025-07-10 16:41:39 +0900

    feat: express.json 및 express.urlencoded 미들웨어 추가로 요청 본문 처리 개선

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,6 @@
 import { NestFactory } from '@nestjs/core';
 import { NestExpressApplication } from '@nestjs/platform-express';
+import * as express from 'express';
 import * as session from 'express-session';
 import { join } from 'path';
 
@@ -8,13 +9,13 @@ import { AppModule } from './app/app.module';
 async function bootstrap() {
   const app = await NestFactory.create<NestExpressApplication>(AppModule);
 
-
-  // app.use(express.urlencoded({ extended: true }))
+  app.use(express.json({ limit: '50mb' }));
+  app.use(express.urlencoded({ extended: true, limit: '50mb' }));
   app.use(
     session({
       secret: process.env.SESSION_SECRET || 'your-secret-key',
       resave: false,
-      saveUninitialized: false
+      saveUninitialized: false,
     }),
   );
 
