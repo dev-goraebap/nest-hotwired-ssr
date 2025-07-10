d29cbaaf6687709d85276b72c9ce3d4f55519415
dev-goraebap <dev.goraebap@gmail.com>
2025-07-10 16:40:44 +0900

    feat: getBySlug 메서드에 언어 코드 매개변수 추가 및 번역 처리 구현

--- src/app/services/documents.service.ts
+++ src/app/services/documents.service.ts
@@ -1,11 +1,18 @@
-
-import { MvcNotFoundException } from "nestjs-mvc-tools";
-import { DocumentEntity } from "../entities/document.entity";
+import { MvcNotFoundException } from 'nestjs-mvc-tools';
+import { DocumentEntity } from '../entities/document.entity';
 
 export class DocumentsService {
-  async getBySlug(slug: string) {
+  async getBySlug(slug: string, lang = 'ko') {
     const result = await DocumentEntity.findOne({
-      where: { slug },
+      where: {
+        slug,
+        translations: {
+          languageCode: lang,
+        },
+      },
+      relations: {
+        translations: true,
+      },
     });
     if (!result) {
       throw new MvcNotFoundException('게시물을 찾을 수 없습니다.');
