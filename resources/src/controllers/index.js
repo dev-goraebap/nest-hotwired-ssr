7937024b1e454fab3206cf7d2ad2e4c169399c09
dev-goraebap <dev.goraebap@gmail.com>
2025-07-11 03:31:35 +0900

    feat: ThemeSwitcherController 추가 및 index.js에 등록

--- resources/src/controllers/index.js
+++ resources/src/controllers/index.js
@@ -4,8 +4,9 @@ import { DocumentFormController } from './admin/documents/form.controller';
 import { FlashController } from './flash.controller';
 import { HelloWorldController } from './hello_world_controller';
 import { ModalController } from './modal.controller';
-import { TinyMceController } from './tiny-mce.controller';
 import { SortableController } from './sortable_controller';
+import { ThemeSwitcherController } from './theme_switcher_controller';
+import { TinyMceController } from './tiny-mce.controller';
 
 application.register('hello', HelloWorldController);
 application.register('tiny-mce', TinyMceController);
@@ -13,3 +14,4 @@ application.register('modal', ModalController);
 application.register('document-form', DocumentFormController);
 application.register('flash', FlashController);
 application.register('sortable', SortableController);
+application.register('theme-switcher', ThemeSwitcherController);
