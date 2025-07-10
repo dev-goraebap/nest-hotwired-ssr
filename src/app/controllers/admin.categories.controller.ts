ae6f50c0fe136f9aa3bdf398b0b5426effe023ce
dev-goraebap <dev.goraebap@gmail.com>
2025-07-10 16:41:30 +0900

    feat: 서비스 메서드 호출을 유스케이스로 변경하여 코드 구조 개선

--- src/app/controllers/admin.categories.controller.ts
+++ src/app/controllers/admin.categories.controller.ts
@@ -18,20 +18,26 @@ import {
   View,
 } from 'nestjs-mvc-tools';
 import { AdminCategoriesService } from 'src/app/services/admin.categories.service';
+import { CreateCategoryUseCase } from '../use-cases/create-category.use-case';
+import { UpdateCategoryUseCase } from '../use-cases/update-category.use-case';
 
 @Controller({ path: 'admin/categories' })
 export class AdminCategoriesController {
-  constructor(private readonly categoriesService: AdminCategoriesService) {}
+  constructor(
+    private readonly categoriesService: AdminCategoriesService,
+    private readonly createCategoryUseCase: CreateCategoryUseCase,
+    private readonly updateCategoryUseCase: UpdateCategoryUseCase,
+  ) {}
 
   @Get()
   async index(@View() view: NestMvcView) {
-    const categories = await this.categoriesService.index();
+    const categories = await this.categoriesService.getAll();
     return view.render('pages/admin/categories/index', { categories });
   }
 
   @Get('modal')
   async modalIndex(@View() view: NestMvcView) {
-    const categories = await this.categoriesService.index();
+    const categories = await this.categoriesService.getAll();
     return view.render('pages/admin/categories/index_turbo_frame', { categories });
   }
 
@@ -43,7 +49,7 @@ export class AdminCategoriesController {
   @Post()
   async create(@Req() req: NestMvcReq) {
     console.log(req.body?.category);
-    await this.categoriesService.create(req.body?.category);
+    await this.createCategoryUseCase.execute(req.body?.category);
 
     req.flash.success('작업 성공');
     return req.view.render('pages/admin/categories/_success');
@@ -51,7 +57,7 @@ export class AdminCategoriesController {
 
   @Get(':id/edit')
   async edit(@Param('id', ParseIntPipe) id: number, @View() view: NestMvcView) {
-    const category = await this.categoriesService.show(id);
+    const category = await this.categoriesService.getById(id);
     return view.render('pages/admin/categories/edit', { category });
   }
 
@@ -64,7 +70,7 @@ export class AdminCategoriesController {
 
   @Put(':id')
   async update(@Param('id', ParseIntPipe) id: number, @Req() req: NestMvcReq) {
-    await this.categoriesService.update(id, req.body?.category);
+    await this.updateCategoryUseCase.execute(id, req.body?.category);
     req.flash.success('작업 성공');
     return req.view.render('pages/admin/categories/_success');
   }
