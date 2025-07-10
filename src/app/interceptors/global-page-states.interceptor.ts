335f6c43b4458e52e5ba8a95206b19b4c83b8de2
dev-goraebap <dev.goraebap@gmail.com>
2025-07-10 16:41:02 +0900

    feat: 카테고리 가져오기 메서드에 언어 코드 매개변수 추가

--- src/app/interceptors/global-page-states.interceptor.ts
+++ src/app/interceptors/global-page-states.interceptor.ts
@@ -27,7 +27,9 @@ export class GlobalPageStatesInterceptor implements NestInterceptor {
     const i18nContext = I18nContext.current();
     const lang = i18nContext?.lang;
 
-    const categories = await this.categoriesService.getSidebarCategories();
+    // 언어 코드를 전달하여 카테고리 가져오기
+    const categories = await this.categoriesService.getSidebarCategories(lang);
+    console.log(categories);
 
     // 전역 상태로 lang 처리
     view.share({ lang, categories });
