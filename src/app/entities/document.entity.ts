ee28ffedca6a1de1750a507a3f9de967c7cab401
dev-goraebap <dev.goraebap@gmail.com>
2025-07-10 16:41:06 +0900

    feat: DocumentEntity에서 title 및 content 속성을 제거하고 번역을 통해 접근하도록 변경

--- src/app/entities/document.entity.ts
+++ src/app/entities/document.entity.ts
@@ -20,12 +20,6 @@ export class DocumentEntity extends BaseEntity {
   @Column({ unique: true })
   readonly slug: string;
 
-  @Column({ unique: true })
-  readonly title: string;
-
-  @Column({ type: 'text' })
-  readonly content: string;
-
   @CreateDateColumn()
   readonly createdAt: Date;
 
@@ -43,6 +37,21 @@ export class DocumentEntity extends BaseEntity {
     cascade: true,
   })
   readonly translations: DocumentTranslationEntity[];
+
+  /**
+   * 서비스를 통해 이미 현재 언어코드조건을 사용했다고 가정
+   */
+  get translation() {
+    return this.translations[0];
+  }
+
+  get title() {
+    return this.translation?.title || '';
+  }
+
+  get content() {
+    return this.translation?.content || '';
+  }
 
   getUrl() {
     if (this.slug.includes('/')) {
