c79a04be51538544c529e77c0d4e25f7a0a87266
dev-goraebap <dev.goraebap@gmail.com>
2025-07-20 14:58:02 +0900

    refactor: AppModule -> MainModule에서 예외 필터 및 인터셉터 설정 이동

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,8 +1,4 @@
 import { Module } from '@nestjs/common';
-import { APP_FILTER, APP_INTERCEPTOR } from '@nestjs/core';
-
-import { AppExceptionFilter } from 'src/common/filters/app-exception.filter';
-import { GlobalPageStatesInterceptor } from 'src/common/interceptors/global-page-states.interceptor';
 
 import { AdminModule } from './admin/admin.module';
 import { PublicModule } from './public/public.module';
@@ -11,10 +7,6 @@ import { PublicModule } from './public/public.module';
   imports: [
     AdminModule,
     PublicModule
-  ],
-  providers: [
-    { provide: APP_FILTER, useClass: AppExceptionFilter },
-    { provide: APP_INTERCEPTOR, useClass: GlobalPageStatesInterceptor },
-  ],
+  ]
 })
 export class AppModule {}
--- src/main.module.ts
+++ src/main.module.ts
@@ -1,17 +1,25 @@
 import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
+import { APP_FILTER, APP_INTERCEPTOR } from '@nestjs/core';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { I18nModule } from 'nestjs-i18n';
 import { NestMvcCoreModule } from 'nestjs-mvc-tools';
 
-import { AppModule } from './app/app.module';
+import { TypeormActiveStorageModule } from './libs/typeorm-active-storage';
+
 import { configOptions } from './config/config.options';
 import { i18nOptions } from './config/i18n.options';
 import { NestjsMvcToolsOptionsImpl } from './config/nestjs-mvc-tools.options';
 import { TypeormActiveStorageOptionsImpl } from './config/typeorm-active-storage.options';
 import { TypeOrmOptionsImpl } from './config/typeorm.options';
-import { TypeormActiveStorageModule } from './libs/typeorm-active-storage';
+
+import { AppExceptionFilter } from './common/filters/app-exception.filter';
+import { GlobalPageStatesInterceptor } from './common/interceptors/global-page-states.interceptor';
+
 import { SharedModule } from './shared';
+
+import { AppModule } from './app/app.module';
+
 
 @Module({
   imports: [
@@ -24,6 +32,10 @@ import { SharedModule } from './shared';
     }),
     SharedModule,
     AppModule,
+  ],
+  providers: [
+    { provide: APP_FILTER, useClass: AppExceptionFilter },
+    { provide: APP_INTERCEPTOR, useClass: GlobalPageStatesInterceptor },
   ],
 })
 export class MainModule {}
